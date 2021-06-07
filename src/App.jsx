f92574f090c23660d690c1c5aea2bc858175e7e2
DevHyup <02ydh@naver.com>
2021-06-07 18:19:05 +0900

    list
    list

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import VideoList from './components/video_list/video_list';
 
 function App() {
+  const API_KEY = process.env.REACT_APP_GOOGLE_OAUTH_API_KEY;
   const [videos, setVideos] = useState([]);
-  const [name, setName] = useState(['hyup']);
 
   useEffect(() => {
-    var requestOptions = {
+    const requestOptions = {
       method: 'GET',
       redirect: 'follow',
     };
-    const API_KEY = process.env.REACT_APP_GOOGLE_OAUTH_API_KEY;
+
     fetch(
       `https://youtube.googleapis.com/youtube/v3/videos/?part=snippet&chart=mostPopular&maxResult=25&key=${API_KEY}`,
       requestOptions
     )
-      .then(response => response.text())
-      .then(result => console.log(result))
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
       .catch(error => console.log('error', error));
   }, []);
-  return <h1>Hello :) </h1>;
+  return <VideoList videos={videos} />;
 }
 
 export default App;
